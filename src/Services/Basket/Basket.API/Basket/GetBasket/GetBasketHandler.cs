65d480891f12ef01253f1a88bfbefec710315ea1
qiuxan <qiuxan@gmail.com>
2024-08-30 20:59:07 +1000

    Create Getbasket handler with a mock result

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,5 +1,16 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketHandler
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShoppingCart Cart);
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        //to do: get basket from db
+
+        //var basket = await _repository.GetBasketAsync(query.UserName);
+
+        return new GetBasketResult(new ShoppingCart("john doe"));
+    }
 }
