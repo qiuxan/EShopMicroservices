eb69aaba10f2f8bade2c3290afcfc12c6d005f40
qiuxan <qiuxan@gmail.com>
2024-08-31 16:15:04 +1000

    Develop BasketRepository Class using Marten Library

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Basket.API.Data;
+
+public class BasketRepository : IBasketRepository
+{
+    public Task<bool> DeleteBasketAsync(string userName, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ShoppingCart> GetBasketAsync(string userName, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ShoppingCart> UpdateBasketAsync(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
