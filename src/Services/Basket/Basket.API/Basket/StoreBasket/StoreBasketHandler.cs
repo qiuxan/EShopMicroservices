2495c5c30ae61165f3a5e73658682bc12fca0425
qiuxan <qiuxan@gmail.com>
2024-08-31 14:53:11 +1000

    create StoreBasketCommandHandler

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,5 +1,31 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿
 
-public class StoreBasketHandler
+namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart):ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator:AbstractValidator<StoreBasketCommand>
 {
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName can not be empty");
+    }
 }
+
+public class StoreBasketCommandHandler:ICommandHandler<StoreBasketCommand,StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        
+        ShoppingCart cart = command.Cart;
+
+        //to do: save basket to db use marten upsert if exists update else insert
+
+        //update cache
+
+        return new StoreBasketResult("john doe");
+    }
+}
+
