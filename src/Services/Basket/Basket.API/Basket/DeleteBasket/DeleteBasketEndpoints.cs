5e587f22be120651085977cc486bd54d279f245b
qiuxan <qiuxan@gmail.com>
2024-08-31 15:39:01 +1000

    Delete basket endpoints

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
@@ -1,5 +1,30 @@
-﻿namespace Basket.API.Basket.DeleteBasket;
+﻿
+using Basket.API.Basket.StoreBasket;
 
-public class DeleteBasketEndpoints
+namespace Basket.API.Basket.DeleteBasket;
+
+
+//public record DeleteBasketRequest(string UserName);
+
+public record DeleteBasketResponse(bool IsSuccess);
+
+public class DeleteBasketEndpoints : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/basket/{userName}", async (string UserName, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteBasketCommand(UserName));
+
+            var response = result.Adapt<DeleteBasketResponse>();
+
+            return Results.Ok(response);
+
+        })
+        .WithName("Delete Basket")
+        .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete Basket")
+        .WithDescription("Delete Basket");
+    }
 }
