3ef5123f5b0aa8ed37c339a5d30ef5bc35b15d34
qiuxan <qiuxan@gmail.com>
2024-08-31 15:30:44 +1000

    create delete basket handler

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,5 +1,24 @@
-﻿namespace Basket.API.Basket.DeleteBasket;
+﻿
+namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketHandler
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName can not be empty");
+    }
+}
+public class DeleteBasketCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        //to do: delete basket from db and cache
+        // session.DeleteBasketAsync(command.UserName);
+
+        return new DeleteBasketResult(true);
+    }
 }
