2d9c0432f8f34d79428dc8be58996af1add80c48
qiuxan <qiuxan@gmail.com>
2024-09-01 13:10:37 +1000

    Register Exception Handling from building blocks in Basket Microservices

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,5 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly;
@@ -21,9 +23,12 @@ builder.Services.AddMarten(options =>
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
 app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 app.Run();
