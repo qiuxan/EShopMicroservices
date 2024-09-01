7fa23e3266f2e8091318e63075d586b993b3756b
qiuxan <qiuxan@gmail.com>
2024-09-01 17:09:12 +1000

    Develop CachedBasketRepository with Proxy Pattern, Decorator Pattern and Scrutor

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Basket.API.Data;
+
+public class CachedBasketRepository
+    (
+        IBasketRepository repository
+    )
+    : IBasketRepository
+{
+    public Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        return repository.GetBasket(userName, cancellationToken);
+    }
+
+    public Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        return repository.StoreBasket(basket, cancellationToken);
+    }
+
+    public Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        return repository.DeleteBasket(userName, cancellationToken);
+    }
+}
