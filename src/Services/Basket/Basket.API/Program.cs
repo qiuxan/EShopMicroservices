4a5d43113e4fcb52b1acbdb6fff91305fc296c7b
qiuxan <qiuxan@gmail.com>
2024-09-02 11:15:16 +1000

    fix some errors
    builder.Services.AddScoped<IBasketRepository called twice

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -6,12 +6,12 @@ var assembly = typeof(Program).Assembly;
 
 // Add services to the container.
 builder.Services.AddCarter();
+
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssemblies(assembly);
+    config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-
 });
 
 builder.Services.AddMarten(options =>
@@ -30,8 +30,6 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = builder.Configuration.GetConnectionString("Redis")!;
     //options.InstanceName = "Basket";
 });
-
-builder.Services.AddScoped<IBasketRepository, CachedBasketRepository>();
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
