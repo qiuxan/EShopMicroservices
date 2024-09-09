bf42f0cbee019a3da2a8ddcddfc805aeb142af88
qiuxan <qiuxan@gmail.com>
2024-09-10 09:32:23 +1000

    Register EF Core DbContext in Asp.Net DependencyInjection Tool for SQL Server + setup global using for infra

--- src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Ordering.Domain.Models;
-using System.Reflection;
-
+﻿
 namespace Ordering.Infrastructure.Data;
 public class ApplicationDbContext: DbContext
 {
--- src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Ordering.Domain.Enums;
-using Ordering.Domain.Models;
-using Ordering.Domain.ValueObjects;
-
-namespace Ordering.Infrastructure.Data.Configurations;
+﻿namespace Ordering.Infrastructure.Data.Configurations;
 public class OrderConfiguration : IEntityTypeConfiguration<Order>
 {
     public void Configure(EntityTypeBuilder<Order> builder)
