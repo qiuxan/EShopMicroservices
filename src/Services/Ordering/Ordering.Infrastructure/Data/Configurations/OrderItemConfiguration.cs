218c97b5c0a7896af3b211efe5f16bd12a5c7228
qiuxan <qiuxan@gmail.com>
2024-09-09 11:19:33 +1000

    Mapping DDD Objects to EF Entities - Develop Order Entity Configurations

--- src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
 
 namespace Ordering.Infrastructure.Data.Configurations;
 internal class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
 {
     public void Configure(EntityTypeBuilder<OrderItem> builder)
     {
-        throw new NotImplementedException();
+        builder.HasKey(oi => oi.Id);
+        builder.Property(oi => oi.Id).HasConversion
+            (
+                orderItemId => orderItemId.Value,
+                dbId => OrderItemId.Of(dbId)
+            );
+        builder.Property(oi=>oi.Price).IsRequired();
+        builder.Property(oi=>oi.Quantity).IsRequired();
+
+        builder.HasOne<Product>()
+            .WithMany()
+            .HasForeignKey(oi => oi.ProductId);
     }
 }
