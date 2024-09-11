a3bab8512728282e63033dc39916689fbfbb19d8
qiuxan <qiuxan@gmail.com>
2024-09-11 17:00:25 +1000

    Seed Product and Orders in SQL Server OrderDb using EF Core when App Startup

--- src/Services/Ordering/Ordering.Infrastructure/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Extensions/DatabaseExtensions.cs
@@ -18,6 +18,17 @@ public static class DatabaseExtensions
     private static async Task SeedAsync(ApplicationDbContext context)
     {
         await SeedCustomerAsync(context);
+        await SeedProductAsync(context);
+        await SeedOrderWithItemsAsync(context);
+    }
+
+    private static async Task SeedProductAsync(ApplicationDbContext context)
+    {
+        if (!await context.Products.AnyAsync())
+        {
+            await context.Products.AddRangeAsync(InitialData.Products);
+            await context.SaveChangesAsync();
+        }
     }
 
     private static async Task SeedCustomerAsync(ApplicationDbContext context)
@@ -25,6 +36,15 @@ public static class DatabaseExtensions
         if (!await context.Customers.AnyAsync())
         {
             await context.Customers.AddRangeAsync(InitialData.Customers);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedOrderWithItemsAsync(ApplicationDbContext context)
+    {
+        if (!await context.Orders.AnyAsync())
+        {
+            await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
             await context.SaveChangesAsync();
         }
     }
