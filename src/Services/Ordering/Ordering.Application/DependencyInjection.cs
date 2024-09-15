3f6fc0ff683617c3ce90870dfe2737b3a878e045
qiuxan <qiuxan@gmail.com>
2024-09-15 14:20:24 +1000

    Develop MediatR Pipeline Behaviours (Validation, Logging) in Ordering.App

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿
+using BuildingBlocks.Behaviors;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -7,9 +8,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        services.AddMediatR(cfg=>
-        { 
-            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(config=>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
         return services;
     }
