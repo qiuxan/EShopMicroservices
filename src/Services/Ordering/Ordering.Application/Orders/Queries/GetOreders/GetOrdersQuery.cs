84a25de43de5a64bb3ac326876c8251769218543
qiuxan <qiuxan@gmail.com>
2024-09-16 12:22:43 +1000

    Develop GetOrders Query apply Pagination using CQRS and MediatR in Ordering.App

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOreders/GetOrdersQuery.cs
@@ -0,0 +1,5 @@
+﻿using BuildingBlocks.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOreders;
+public record GetOrdersQuery(PaginationRequest PaginationRequest) : IQuery<GetOrdersResult>;
+public record GetOrdersResult(PaginatedResult<OrderDto> Orders);
