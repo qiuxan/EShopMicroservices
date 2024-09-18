a11fb31f4f6e665498c4452f8fd637d53562011e
qiuxan <qiuxan@gmail.com>
2024-09-18 15:32:34 +1000

    fix delete end point typo

--- src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -2,7 +2,7 @@
 
 namespace Ordering.Application.Orders.Commands.DeleteOrder;
 public record DeleteOrderCommand(Guid OrderId): ICommand<DeleteOrderResult>;
-public record DeleteOrderResult(bool isSuccess);
+public record DeleteOrderResult(bool IsSuccess);
 
 
 public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
