1ae8d8e86b043528ced918a01080e5c3c62eaa1d
qiuxan <qiuxan@gmail.com>
2024-09-18 16:43:55 +1000

    Health Checks for Ordering Microservices with EF.Core and SQL Server

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,14 +1,19 @@
 ﻿
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API;
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddCarter();
 
         services.AddExceptionHandler<CustomExceptionHandler>();
+
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
 
         return services;
     }
@@ -18,6 +23,12 @@ public static class DependencyInjection
         app.MapCarter();
 
         app.UseExceptionHandler(options => { });
+
+        app.UseHealthChecks("/health",
+            new HealthCheckOptions 
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
 
         return app;
     }
