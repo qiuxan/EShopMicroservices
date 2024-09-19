c7dd9a86538f32c6ab66d10cf78215d711f7d8df
qiuxan <qiuxan@gmail.com>
2024-09-19 16:00:00 +1000

    Develop BasketCheckout Event in BuildingBlocks.Messaging Shared Class Library

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.Messaging.Events;
+public record IntegrationEvent 
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public DateTime OccurredOn = DateTime.UtcNow;
+    public string EventType =>GetType().AssemblyQualifiedName;
+}
