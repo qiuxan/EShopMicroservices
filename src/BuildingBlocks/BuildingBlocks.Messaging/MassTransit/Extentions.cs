72cdc8fb27f52aa1bc3f0b0fb33c43cfba08f668
qiuxan <qiuxan@gmail.com>
2024-09-20 06:50:44 +1000

    Develop MassTransit Extention Methods to Register RabbitMQ connection into DI -2

--- src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace BuildingBlocks.Messaging.MassTransit;
 public static class Extentions
 {
-    public static IServiceCollection AddMessageBroker(this IServiceCollection services, Assembly? assembly)
+    public static IServiceCollection AddMessageBroker(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        Assembly? assembly = null
+        )
     {
-        // todo Implement RabbitMQ MassTransit configuration
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly != null)
+                config.AddConsumers(assembly);
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:UserName"]);
+                    host.Password(configuration["MessageBroker:Password"]);
+                });
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+
         return services;
     }
 }
