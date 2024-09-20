d27590783fc11d3977614468eaec8fafb9b1ac95
qiuxan <qiuxan@gmail.com>
2024-09-20 11:01:53 +1000

    Develop Basket Checkout Handler Method to Publish event to RabbitMQ-MassTransit

--- src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
@@ -1,5 +1,45 @@
-﻿namespace Basket.API.Basket.CheckoutBasket;
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
 
-public class CheckoutBasketHandler
+namespace Basket.API.Basket.CheckoutBasket;
+public record CheckoutBasketCommand(BasketCheckoutDto BasketCheckoutDto)
+    :ICommand<CheckoutBasketResult>;
+public record CheckoutBasketResult(bool IsSuccess);
+
+public class CheckoutBasketCommandValitator
+    : AbstractValidator<CheckoutBasketCommand>
 {
+    public CheckoutBasketCommandValitator()
+    {
+        RuleFor(x => x.BasketCheckoutDto).NotNull().WithMessage("BasketCheckout Dto can not be null");
+        RuleFor(x => x.BasketCheckoutDto.UserName).NotNull().WithMessage("UserName can not be null");
+    }
+}
+public class CheckoutCommandBasketHandler
+    (IBasketRepository basketRepository, IPublishEndpoint publishEndpoint)
+    :ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+{
+    public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+    {
+        //get existing basket with total price
+        var basket = await basketRepository.GetBasket(command.BasketCheckoutDto.UserName,cancellationToken);
+        if(basket == null)
+        {
+           return new CheckoutBasketResult(false);
+        }
+
+        var eventMessage = command.BasketCheckoutDto.Adapt<BasketCheckoutEvent>();
+
+        //set total price on basketcheckout event message
+
+        eventMessage.TotalPrice = basket.TotalPrice;
+
+        //send basketcheckout eveent to rabbitmq using masstransit
+
+        await publishEndpoint.Publish(eventMessage,cancellationToken);
+        //delete basket
+        await basketRepository.DeleteBasket(command.BasketCheckoutDto.UserName,cancellationToken);
+
+        return new CheckoutBasketResult(true);
+    }
 }
